fb7600bbdb4c5fe52c448cfa4f335e28e0b60626
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-12-25 22:03:27 +0530

    created backend for TaskLink

---
+++ backend/tasklink/tasklink/src/main/java/com/tasklink/tasklink/TasklinkApplication.java
@@ -0,0 +1,13 @@
+package com.tasklink.tasklink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklinkApplication.class, args);
+	}
+
+}
